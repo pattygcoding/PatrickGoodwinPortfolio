c4ec221b4245dbedd7eb1d1094407c4d119d99b9
phgoodw <75592933+phgoodw@users.noreply.github.com>
2024-04-04 11:16:25 -0400

    Push to GitHub

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "PATRICK";
+const meta = {
+    title: "Patrick Goodwin",
+    description: "Orlando-based Full Stack Software Engineer",
+};
+
+const introdata = {
+    title: "Patrick Goodwin",
+    animated: {
+        first: "Full Stack Software Engineer",
+        second: "Web Developer",
+        third: "Game Developer",
+        fourth: "Content Creator",
+    },
+    descriptionA: "Experienced full-stack software engineer & web developer with a demonstrated history of successfully leading development projects, from creating Java modifications for Minecraft to enhancing user experience in React.js and implementing secure REST APIs, while consistently employing agile methodologies and effective team collaboration.",
+    descriptionB: "Additionally, I am a geography content creator who has successfully leveraged the algorithms of multiple social media platforms, including Instagram, YouTube, and TikTok, to gain a respectable and engaging follower base.",
+
+};
+
+const dataabout = {
+    title: "Personal Summary",
+    aboutme: "As an experienced Full Stack Software Developer based in Orlando, FL, I've demonstrated expertise in various technologies, from Java and C++ to React.js and Node.js. With a track record of spearheading projects, including the development of a Java modification program for Minecraft and a rewarding website for truck drivers, I've showcased leadership and innovation. My commitment to excellence is evident in my ability to streamline project management, solve complex problems, and deliver high-quality solutions that enhance user experience and drive organizational success.",
+};
+const worktimeline = [{
+        jobtitle: "Founder & Full Stack Software Developer",
+        where: "SupremeMC",
+        date: "2020 - Present",
+    },
+    {
+        jobtitle: "Full Stack Software Engineer",
+        where: "Accelerate Learning",
+        date: "2023",
+    },
+    {
+        jobtitle: "Lead Web Developer (Student Program)",
+        where: "Clemson University",
+        date: "2021 - 2022",
+    },
+    {
+        jobtitle: "Desk Supervisor",
+        where: "Clemson University",
+            date: "2021 - 2022",
+    },
+];
+
+const skills = [{
+        name: "Java",
+    },
+    {
+        name: "JavaScript",
+    },
+    {
+        name: "TypeScript",
+    },
+    {
+        name: "C",
+    },
+    {
+        name: "C++",
+    },
+    {
+        name: "C#",
+    },
+    {
+        name: "PHP",
+    },
+    {
+        name: "Python",
+    },
+    {
+         name: "React",
+    },
+    {
+         name: "Node.js",
+    },
+    {
+         name: "Gradle",
+    },
+    {
+         name: "JSON",
+    },
+    {
+         name: "SQL",
+    },
+    {
+         name: "Jira",
+    },
+    {
+         name: "Confluence",
+    },
+    {
+         name: "Azure DevOps",
+    },
+    {
+         name: "HTML",
+    },
+    {
+         name: "CSS",
+    },
+    {
+         name: "Git",
+    },
+    {
+         name: "Linux",
+    },
+];
+
+const services = [{
+        title: "Full Stack Engineering",
+        description: " I offer comprehensive solutions across both front-end and back-end development, ensuring seamless integration and functionality throughout the entire software stack.",
+    },
+    {
+        title: "Web Development",
+        description: "With expertise in HTML, CSS, and JavaScript frameworks like React.js, I craft visually appealing and user-friendly websites tailored to meet clients' specific needs and preferences.",
+    },
+    {
+        title: "Game Development",
+        description: "Leveraging languages such as Java and C++, I specialize in creating immersive gaming experiences, from concept and design to implementation and optimization, ensuring engaging gameplay and smooth performance.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.imgur.com/NvXTuC1.jpg",
+        description: "SupremeMC: The professional game extension for Minecraft: Java Edition Version 1.20. Built in Java 17 with Gradle.",
+        suprememc_website: "https://www.curseforge.com/minecraft/mc-mods/suprememc",
+        suprememc_linkedin: "https://www.linkedin.com/company/suprememc",
+        suprememc_github: "https://github.com/pattygcoding/SupremeMC",
+    },
+    {
+        img: "https://i.imgur.com/l0beInX.jpeg",
+        description: "SupremeMC Legacy: The 1.16 Version for SupremeMC. Built in Java 8 with Gradle.",
+        suprememc_website: "https://www.curseforge.com/minecraft/mc-mods/suprememc",
+        suprememc_github_legacy: "https://github.com/pattygcoding/SMC",
+    },
+    {
+        img: "https://i.imgur.com/KI71fuk.jpeg",
+        description: "This Website: I created this website myself using JavaScript & React.",
+        website_github: "https://github.com/pattygcoding/PatrickGoodwinPortfolio",
+    },
+];
+
+const contactConfig = {
+    email: "pattygsocials@gmail.com",
+    description: "I respond to inquires from email, LinkedIn messages, and Instagram messages. I also will call back if my number is provided via resume or otherwise, though I prefer to keep my phone number private.",
+};
+
+const socialprofils = {
+    github: "https://github.com/pattygcoding",
+    instagram: "https://www.instagram.com/patty_geography",
+    linkedin: "https://www.linkedin.com/in/patrickgoodwin7",
+    tiktok: "https://www.tiktok.com/@patty_g7",
+    youtube: "https://www.youtube.com/@patty_g7",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
